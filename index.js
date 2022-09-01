9ea21d701279f44a838f66ea9c2801beaf53cdd4
0xv0idx <serkandayicik@gmail.com>
2022-09-02 01:07:54 +0300

    response code added

--- index.js
+++ index.js
@@ -1,29 +1,45 @@
 const axios = require("axios")
-const { EmbedBuilder, WebhookClient } = require('discord.js');
+const { EmbedBuilder, WebhookClient } = require('discord.js')
+
+// thnx m6_SOL for response code https://twitter.com/m6_SOL/status/1565457692231192583
 
 const wallet = "walletaddress"
-const discordId = "discorduserid"
+const discordWebhookUrl = ""
+const discordUserId = "discorduserid"
 
 
 function checky00ts() {
+    const webhookClient = new WebhookClient({ url: discordWebhookUrl })
 
     axios.get(`https://api.degods.com/scholarships/application/status?solPubkey=${wallet}`)
     .then(function (response) {
-        if (response.data.status != 0) {
-
-            const webhookClient = new WebhookClient({ url: "https://discord.com/api/webhooks" });
-            
-            const embed = new EmbedBuilder()
-                .setTitle('y00ts list status')
-                .setDescription(`STATUS: ${response.data.status}`)
+        switch(response.data.status) {
+            case 1:
+                // passed
+                const embedPassed = new EmbedBuilder()
+                .setTitle('y00ts aplication accepted !')
                 .setColor(0x26ff00);
-            
-            webhookClient.send({
-                content: `<@${discordId}>`,
-                username: 'Y00TS',
-                avatarURL: 'https://i.imgur.com/AfFp7pu.png',
-                embeds: [embed],
-            });
+
+                webhookClient.send({
+                    content: `<@${discordUserId}>`,
+                    username: 'Y00TS',
+                    avatarURL: 'https://i.imgur.com/AfFp7pu.png',
+                    embeds: [embedPassed],
+                });
+                break;
+            case 2:
+                // didnt pass
+                const embed = new EmbedBuilder()
+                .setTitle('y00ts aplication rejected :(')
+                .setColor(0x26ff00);
+
+                webhookClient.send({
+                    content: `<@${discordUserId}>`,
+                    username: 'Y00TS',
+                    avatarURL: 'https://i.imgur.com/AfFp7pu.png',
+                    embeds: [embed],
+                });
+                break;
         }
     })
     .catch(function (error) {
